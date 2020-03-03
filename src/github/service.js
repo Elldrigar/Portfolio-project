836282fe3f77b6043bc587beb595f7e0ff2a4384
Artur Gawron <artur@gawron.me>
2020-03-03 15:16:08 +0100

    Add converter usage

--- src/github/service.js
+++ src/github/service.js
@@ -1,9 +1,15 @@
 import {GitHubRepo} from "./model";
 
 const reposURL = 'https://api.github.com/users/Elldrigar/repos';
-const forbiddenRepos = ['Portfolio-project','JSVanilla_Infinite-scroll-posts'];
-const convert = ({name, stargazers_count: stars, license
-                 }) => new GitHubRepo({name, stars, license: license ? license.spdx_id : ''
+const forbiddenRepos = ['Portfolio-project', 'JSVanilla_Infinite-scroll-posts'];
+const convert = ({
+    name,
+    stargazers_count: stars,
+    license
+}) => new GitHubRepo({
+    name,
+    stars,
+    license: license ? license.spdx_id : ''
 });
 
 export default function getRepos() {
@@ -14,6 +20,8 @@ export default function getRepos() {
             }
             throw Error('Response is not 200');
         })
-            .catch(err => console.warn(err))
-        .then(arr => arr.filter(r => !forbiddenRepos.includes(r.name)));
+        .then(arr => arr
+            .filter(r => !forbiddenRepos.includes(r.name))
+            .map(convert))
+        .catch(err => console.warn(err))
 }
