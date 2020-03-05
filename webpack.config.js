4d60826b9b7c0f72b466ba0cc8ac41340d2d0110
Artur Gawron <artur@gawron.me>
2020-03-05 13:49:18 +0100

    Add Webpack plugin:Copy and Clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const copyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     plugins: [
+        new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
-        })
+        }),
+        new copyPlugin([
+            {from: 'src/assets/', to: 'assets'}
+        ])
     ]
 };
