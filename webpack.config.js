7d32c5d8c1f5b324201f59c60fe32633e8630ae3
Artur Gawron <artur@gawron.me>
2020-03-06 23:25:13 +0100

    Add clean plugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const copyPlugin = require('copy-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
