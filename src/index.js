94f68453e70dde105654590dc9673d368be019e0
Artur Gawron <artur@gawron.me>
2020-03-08 16:37:00 +0100

    Add three monkey buttons 🙉🙈🙊

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import initMD from 'markdown-element';
 import { getBlogPost } from "./github/service";
-// import game from './game/index';
-// import main from './github/index';
-import main from './joke/index';
+import gameStart from './game/index';
+import main from './github/index';
+import jokeStart from './joke/index';
 
 import initInfo from './about-me/index';
 import initBlog from './blog/index';
 
 initInfo();
-initBlog();
 
-// main();
+const game = document.querySelector('.game');
+const joke = document.querySelector('.joke');
 
+game.addEventListener('click', gameStart);
+joke.addEventListener('click', jokeStart);
